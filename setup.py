c29b3c8c33770844c06026c3247f2c1d790db467
fratim <frtim@student.ethz.ch>
2021-12-13 14:32:08 +0000

    updated torch package requirements

--- setup.py
+++ setup.py
@@ -5,8 +5,7 @@ setup(
     version="1.1.0",
     keywords="reinforcement learning, actor-critic, a2c, ppo, multi-processes, gpu",
     packages=find_packages(),
-    #install_requires=[
-    #    "numpy>=1.13.0",
-    #    "torch>=1.0.0"
-    #]
+    install_requires=[
+        "torch>=1.0.0"
+    ]
 )
